9b12047e4f821fe9f42b4b80e119c44c28e07c94
DaehyunJoo <dd70296@gmail.com>
2021-11-02 11:13:45 +0900

    feat: NotFound 페이지 추가

---
+++ src/pages/not-found.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="not-found-page-root">
+    <h1 class="code">🚫 404</h1>
+    <h1 class="message">Not Found</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.not-found-page-root {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+
+  & > .code {
+    font-size: 72px;
+  }
+
+  & > .message {
+    font-size: 48px;
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 const LoginPage = () => import('@/pages/login.vue')
 const JoinPage = () => import('@/pages/join.vue')
 const PostsPage = () => import('@/pages/posts.vue')
+const NotFoundPage = () => import('@/pages/not-found.vue')
 
 const routes = [
     { path: '/', component: LoginPage },
     { path: '/join', component: JoinPage },
     { path: '/posts', component: PostsPage },
+    { path: '/:notSemantic(.*)', component: NotFoundPage },
 ];
 
 const router = createRouter({
